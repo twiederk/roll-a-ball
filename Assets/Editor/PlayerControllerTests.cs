5c92a068ab7f86bfde3135ca1d68975707f3f5d6
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2018-10-13 05:36:52 +0200

    First dummy unit test

---
+++ Assets/Editor/PlayerControllerTests.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NUnit.Framework;
+
+public class PlayerControllerTest {
+
+    [Test]
+    public void MyFirst_Test()
+    {
+        Assert.AreEqual(true, false);
+    }
+
+}
