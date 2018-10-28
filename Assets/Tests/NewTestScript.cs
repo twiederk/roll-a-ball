6b3f907e24d435145f1af7a39f8f2337131ecdc4
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2018-10-28 05:03:08 +0100

    Added NSubstitute to the project.

---
+++ Assets/Tests/NewTestScript.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using NSubstitute;
+using System.Collections;
+
+public class NewTestScript {
+
+    [Test]
+    public void NewTestScriptSimplePasses() {
+        // Arrange
+        var weaponStub = Substitute.For<IWeapon>();
+        weaponStub.Fire().Returns(2);
+
+        // Act
+        int fire = weaponStub.Fire();
+
+        // Assert
+        Assert.AreEqual(2, fire);
+        weaponStub.Received().Fire();
+
+
+    }
+
+    // A UnityTest behaves like a coroutine in PlayMode
+    // and allows you to yield null to skip a frame in EditMode
+    [UnityTest]
+    public IEnumerator NewTestScriptWithEnumeratorPasses() {
+        // Use the Assert class to test conditions.
+        // yield to skip a frame
+        yield return null;
+    }
+}
+
+public interface IWeapon
+{
+    int Fire();
+}
+
+
